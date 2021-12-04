fe0e75c49f768f7ad43034f9aa8cd101add2fb28
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-12-04 10:49:27 +0530

    making latest recep

---
+++ components/navbar.js
@@ -0,0 +1,9 @@
+function navbar(){
+    return `<div><img src="https://img.favpng.com/18/10/10/fruit-logo-food-recipe-business-png-favpng-HUvujmv3a23dftJfpeGxyFeaq.jpg"></div>
+    <div onclick="indexPage()">Home</div>
+    <div onclick="recipeofthedayPage()">Recipe of The Day</div>
+    <div onclick="latestPage()">Latest Recipe</div>   
+    <div>About</div> `
+}
+
+export default navbar
